609be865e1f0b5f5b4bfb72ca12d7ac33756531d
Dawid Wasowski <50396440+DawidWasowski@users.noreply.github.com>
2020-08-05 22:06:55 +0200

    Scripts part 1
    Player Movement and partialy Game Manager implemented

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Vertical") * speed);
+        if(Input.GetAxis("Start")>0.0f)
+        {
+            //Start ball
+        }
+    }
+}
