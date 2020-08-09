5ea13dc0e5f5b3bce247c072ad804033ecffb337
Dawid Wasowski <50396440+DawidWasowski@users.noreply.github.com>
2020-08-09 14:33:29 +0200

    UI Scaling to different resolution ratios

---
+++ Assets/Scripts/CameraFOVScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFOVScaler : MonoBehaviour
+{
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        float width = Screen.width;
+        float height = Screen.height;
+        float ratio = width / height;
+        cam.orthographicSize = (7.5f + ratio) / ratio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
